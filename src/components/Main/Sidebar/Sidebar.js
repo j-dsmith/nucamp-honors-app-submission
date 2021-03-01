416b860fe407d7a2247b8e9b2dfeaf35505002e7
Jamey Smith <smith.jameyd@gmail.com>
2021-02-28 16:25:46 -0800

    Sidebar Tray added, absolute positioning setup for sidebar tray animation

--- src/components/Main/Sidebar/Sidebar.js
+++ src/components/Main/Sidebar/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {
+  SidebarContainer,
   StyledSidebar,
+  SidebarTray,
   SidebarLabel,
   SidebarIcon,
   SidebarItem,
@@ -9,17 +11,20 @@ import { SidebarData } from "./SidebarData";
 
 const Sidebar = (props) => {
   return (
-    <StyledSidebar>
-      {/* Map over sidebar data to fill sidebar */}
-      {SidebarData.map(({ title, path, icon }, index) => {
-        return (
-          <SidebarItem key={index}>
-            <SidebarIcon>{icon}</SidebarIcon>
-            <SidebarLabel>{title}</SidebarLabel>
-          </SidebarItem>
-        );
-      })}
-    </StyledSidebar>
+    <SidebarContainer>
+      <StyledSidebar>
+        {/* Map over sidebar data to fill sidebar */}
+        {SidebarData.map(({ title, path, icon }, index) => {
+          return (
+            <SidebarItem key={index}>
+              <SidebarIcon>{icon}</SidebarIcon>
+              <SidebarLabel>{title}</SidebarLabel>
+            </SidebarItem>
+          );
+        })}
+      </StyledSidebar>
+      <SidebarTray className="active" />
+    </SidebarContainer>
   );
 };
 
--- src/components/Main/Sidebar/Sidebar.styles.js
+++ src/components/Main/Sidebar/Sidebar.styles.js
@@ -1,19 +1,34 @@
 import styled from "styled-components";
 
-export const StyledSidebar = styled.div`
-  margin: 10px;
-  background-color: #333;
+export const SidebarContainer = styled.div`
   grid-row: 1 / span 8;
   grid-column: 1 / span 1;
-  border-radius: 15px;
+  position: relative;
+`;
+
+export const StyledSidebar = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 250px;
+  background-color: #333;
+  border-radius: 0px 15px 15px 0px;
   transition: all 0.5s ease;
   box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
     0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
     0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
     0 100px 80px rgba(0, 0, 0, 0.12);
+`;
 
-  &:hover {
-    transform: scale(1.01);
+export const SidebarTray = styled.div`
+  position: absolute;
+  background-color: #5e5e5e;
+  z-index: -1;
+  border-radius: 15px;
+  width: 250px;
+  height: 100%;
+
+  &.active {
+    width: 500px;
   }
 `;
 
@@ -23,6 +38,7 @@ export const SidebarItem = styled.div`
   align-items: center;
   height: 50px;
   padding: 0px;
+  cursor: pointer;
 `;
 
 export const SidebarLabel = styled.div`
